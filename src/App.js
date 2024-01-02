e7704e4b317f16b933fdd84f57ffe9b45cc27ca5
Alessandro Iacovella <alessandroiacovella95@gmail.com>
2024-01-02 17:54:44 +0100

    add todolist

--- src/App.js
+++ src/App.js
@@ -1,56 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import { React, useState, useEffect, useRef } from "react";
+import "./App.css";
 
+const initTodos = [
+  {
+    name: "Call my mom",
+    dueDate: new Date().toLocaleDateString(),
+    user_id: 1,
+  },
+
+  {
+    name: "Go to school",
+    dueDate: new Date().toLocaleDateString(),
+    user_id: 2,
+  },
+
+  {
+    name: "Do homework",
+    dueDate: new Date().toLocaleDateString(),
+    user_id: 3,
+  },
+];
 function App() {
+  const [todos, setTodos] = useState([]);
+  const todoEl = useRef("");
+  useEffect(() => {
+    setTodos(initTodos);
+
+    return () => {};
+  }, [todos]);
+
+  const addTodo = (e) => {
+    // proprietà current l'elemento a cui facciamo rifermiento in quel momento
+    // proprietà value per prendere il valore dell'elemento
+    // alert(todoEl.current.value);
+    e.preventDefault();
+    const newTodo = {
+      name: todoEl.current.value,
+      user_id: 4,
+      dueDate: new Date().toLocaleDateString(),
+    };
+    setTodos([newTodo, ...todos]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div className="App container-fluid">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <h1>My TodoList</h1>
+          <form className="my-4">
+            <div className="form-group">
+              <input
+                ref={todoEl}
+                id="todo"
+                name="todo"
+                className="form-field"
+              />
+              <button onClick={addTodo} className=" ms-1 btn btn-success">
+                Add
+              </button>
+            </div>
+          </form>
+          <ul className="list-group">
+            {todos.map((todo) => (
+              <li key={todo.user_id} className="list-group-item">
+                {todo.name}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./index.css";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
+    {/* il provider serve per avvolgere tutti i componenti a cui vogliamo far utilizzare lo store */}
     <Provider store={store}>
       <App />
     </Provider>
